65031d3291b7b504501230489c2b6704a63e042b
Ramesh2248 <76659766+Ramesh2248@users.noreply.github.com>
2021-09-06 08:19:13 +0530

    adding

--- big2.c
+++ big2.c
@@ -4,7 +4,7 @@
 void big2()
 {
     int num1, num2;
-<!-- Adding line for webhook testing -->
+<!-- Adding line for webhook testing 1-->
     printf("\n Enter two integers :\n");
     scanf("%d%d", &num1, &num2);
 
