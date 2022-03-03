c9c63da5f5bd3266d6d1e89e06e0dc305cadad1d
Ramesh2248 <76659766+Ramesh2248@users.noreply.github.com>
2022-03-03 07:49:47 +0530

    Added

--- big3.c
+++ big3.c
@@ -4,7 +4,7 @@ void big3() {
     double n1, n2, n3;
     printf("\n Enter three different numbers\n: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
-
+<!--Adding line for testing webhook -->
     // if n1 is greater than both n2 and n3, n1 is the largest
     if (n1 >= n2 && n1 >= n3)
         printf("%.2f is the largest number.", n1);
